8441cf5ba45dc9b0f8239421da9226711ce574df
Grace Casale <gracecasale@gmail.com>
2018-11-07 19:00:00 -0500

    created a navbar and included bootstrap and font-awesome with our project

--- src/App.js
+++ src/App.js
@@ -5,7 +5,52 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>I like cheese, too!</h1>
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">Dribbble</a>
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Shots <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Designers</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Teams</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Community</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Jobs</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Hiring</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <i className="fa fa-ellipsis-h" aria-hidden="true"></i>
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Sign Up</a>
+
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Sign In</a>
+
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input className="form-control mr-sm-2" placeholder="Search" aria-label="Search" />
+            </form>
+          </nav>
+          <section>Call to Action</section>
+          <section>More info</section>
+        </header>
+
       </div>
     );
   }
