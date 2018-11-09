193c9a98aeb59c654dc29d6180e44fae1c9363eb
Grace Casale <gracecasale@gmail.com>
2018-11-08 19:00:47 -0500

    extracted a full page view layout component - this was the first subfolder important

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import Main from './Main';
+import FullPageView from './layouts/FullPageView';
+
 
 class App extends Component {
   render() {
     return (
-      <div style={{ height: '100vh' }}>
+      <FullPageView>
         <Header />
         <Main />
-      </div>
+      </FullPageView>
     );
   }
 }
