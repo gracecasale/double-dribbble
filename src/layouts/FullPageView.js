0d37b7efbab3a6cafbdf11d9006364f971a93c98
Grace Casale <gracecasale@gmail.com>
2018-11-08 20:05:04 -0500

    added proptypes for full page view and cleaned up app.js

--- src/layouts/FullPageView.js
+++ src/layouts/FullPageView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function FullPageView(props) {
     return (
@@ -7,5 +8,9 @@ function FullPageView(props) {
       </div>
     );
   }
+
+  FullPageView.propTypes = {
+      children: PropTypes.array.isRequired
+  };
 
   export default FullPageView;
